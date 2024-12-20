ce5accc1afe014e9cdc32e8a7e0105bfba3d724d
nidhithewebhub01 <nidhithewebhub01@gmail.com>
2024-12-20 10:37:55 +0530

    entry_widget

---
+++ lib/widgets/exit/exit_widget.dart
@@ -0,0 +1,230 @@
+import 'package:algodynamic/constants/custom_dropdown/custom_drop_down.dart';
+import 'package:algodynamic/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+Column exitWidget() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      const SizedBox(
+        height: 10,
+      ),
+      Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Stop Loss %",
+                  style: TextStyle(fontSize: 15, color: appcolors.whiteColor),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  height: 40,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.only(left: 15),
+                        fillColor: const Color(0xFF0E0E0E),
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide:
+                                const BorderSide(color: Color(0xFF292929))),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide:
+                                const BorderSide(color: Color(0xFF292929))),
+                        disabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide:
+                                const BorderSide(color: Color(0xFF292929))),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide:
+                                const BorderSide(color: Color(0xFF292929)))),
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            width: 15,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Target Profit %",
+                  style: TextStyle(fontSize: 15, color: appcolors.whiteColor),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  height: 40,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.only(left: 15),
+                        fillColor: const Color(0xFF0E0E0E),
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide:
+                                const BorderSide(color: Color(0xFF292929))),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide:
+                                const BorderSide(color: Color(0xFF292929))),
+                        disabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide:
+                                const BorderSide(color: Color(0xFF292929))),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide:
+                                const BorderSide(color: Color(0xFF292929)))),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+      const SizedBox(
+        height: 15,
+      ),
+      Text(
+        "Exit Conditions",
+        style: TextStyle(fontSize: 15, color: appcolors.whiteColor),
+      ),
+      const SizedBox(
+        height: 15,
+      ),
+      Container(
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        width: double.infinity,
+        decoration: BoxDecoration(
+            color: const Color(0xFF0E0E0E),
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(color: const Color(0xFF292929))),
+        child: Center(
+          child: Text(
+            "+ Add a Condition",
+            style: TextStyle(fontSize: 12, color: appcolors.appColor),
+          ),
+        ),
+      ),
+      Row(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                Text(
+                  "Trailing SL% (Optional)",
+                  style: TextStyle(color: Color(0xFF9D9D9D), fontSize: 10),
+                ),
+                SizedBox(
+                  height: 40,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.only(left: 15),
+                        fillColor: const Color(0xFF0E0E0E),
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide:
+                                const BorderSide(color: Color(0xFF292929))),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide:
+                                const BorderSide(color: Color(0xFF292929))),
+                        disabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide:
+                                const BorderSide(color: Color(0xFF292929))),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide:
+                                const BorderSide(color: Color(0xFF292929)))),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Text(
+                  "TPSL Type",
+                  style: TextStyle(color: Color(0xFF9D9D9D), fontSize: 10),
+                ),
+                // SizedBox(
+                //   height: 40,
+                //   child: TextField(
+                //     decoration: InputDecoration(
+                //         contentPadding: const EdgeInsets.only(left: 15),
+                //         fillColor: const Color(0xFF0E0E0E),
+                //         filled: true,
+                //         enabledBorder: OutlineInputBorder(
+                //             borderRadius: BorderRadius.circular(8),
+                //             borderSide:
+                //                 const BorderSide(color: Color(0xFF292929))),
+                //         focusedBorder: OutlineInputBorder(
+                //             borderRadius: BorderRadius.circular(8),
+                //             borderSide:
+                //                 const BorderSide(color: Color(0xFF292929))),
+                //         disabledBorder: OutlineInputBorder(
+                //             borderRadius: BorderRadius.circular(8),
+                //             borderSide:
+                //                 const BorderSide(color: Color(0xFF292929))),
+                //         border: OutlineInputBorder(
+                //             borderRadius: BorderRadius.circular(8),
+                //             borderSide:
+                //                 const BorderSide(color: Color(0xFF292929)))),
+                //   ),
+                // ),
+                customDropDown(
+                  "Candlestick",
+                  () {
+                    showMenu<String>(
+                      color: appcolors.blackColor,
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(color: Color(0xFF292929)),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      context: context,
+                      position: RelativeRect.fromLTRB(
+                        10, // Left margin to align closer to the left
+                        420, // Top margin (controls vertical position)
+                        MediaQuery.of(context).size.width -
+                            50, // Right alignment pushing menu left
+                        0, // Bottom
+                      ),
+                      items: chartTypemenuOptions
+                          .map((option) => PopupMenuItem<String>(
+                                value: option,
+                                child: Center(
+                                  child: Text(
+                                    option,
+                                    style:
+                                        TextStyle(color: appcolors.whiteColor),
+                                  ),
+                                ),
+                              ))
+                          .toList(), // Convert to a list
+                      elevation: 8.0,
+                    );
+                  },
+                ),
+              ],
+            ),
+          )
+        ],
+      )
+    ],
+  );
+}
